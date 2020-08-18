7a1dbb9c5383da59272c2b9a3d7baa6c2f806615
Yuu Tanaka <yutanadreamfilel.seed@gmail.com>
2020-08-18 13:01:38 +0000

    update sample.php

--- sample.php
+++ sample.php
@@ -6,7 +6,7 @@
     </head>
     <body>
         <h1>おみくじ</h1>
-        <p>おみくじの結果：無し</p>
+        <p>おみくじの結果<?php print htmlspecialchars($result, ENT_QUOTES, "UTF-8"); ?></p>
         <form action="sample.php" method="POST">
             <button type="submit" name="act" value="draw">おみくじをひく！</button>
         </form>
